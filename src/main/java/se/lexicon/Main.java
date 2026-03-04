a5abffff9cdb37645a0200a227d41f9da824180c
SMASobur <sikdar.se@gmail.com>
2026-03-04 23:08:55 +0100

    Date and Time exercise from 1 to 4

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,17 +1,45 @@
 package se.lexicon;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        // Exercise 1 – Current Date
+        currentDate();
+        // Exercise 2 – Formatted Current Date
+        formattedDate();
+        // Exercise 3 – Last Monday and the Entire Week
+        lastMondayWeek();
+        // Exercise 4 – Parse Date from String
+        parseDate("2023-12-25");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    }
+    //Create a LocalDate of the current day and print it out.
+    public static void currentDate(){
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+
+    //Create a LocalDate of the current day and print it out in the following pattern using DateTimeFormatter: Torsdag 29 mars.
+    public static void formattedDate(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("eeee dd MMMM");
+        LocalDate date = LocalDate.now();
+        System.out.println(date.format(formatter));
+    }
+
+    //Create a LocalDate of last Monday. Then print out the entire week in a loop using standard ISO format.
+    public static void lastMondayWeek(){
+        LocalDate lastMonday = LocalDate.now().minusDays(1);
+        System.out.println("Last Monday: "+lastMonday);
+        for (int i=0; i<7; i++) {
+            System.out.println(lastMonday.plusDays(i) + " " +lastMonday.plusDays(i).getDayOfWeek() );
         }
+    }
+
+    //Create a LocalDate object from a String by using the .parse() method.
+    public static void parseDate(String date){
+        LocalDate parsedDate = LocalDate.parse(date);
+        System.out.println("Parsed Date: " + parsedDate);
     }
 }
